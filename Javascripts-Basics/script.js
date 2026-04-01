61e897f4d640cdc8987613d2c2320c8671bb9bec
HkyWis <hengkywisianto@gmail.com>
2026-04-01 19:41:21 +0700

    Add JavaScripts Basics

---
+++ Javascripts-Basics/script.js
@@ -0,0 +1,59 @@
+// VARIABLES
+// var
+var name = "Hengky";
+
+// let
+let age = 21;
+
+// const
+const isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+
+// DATA TYPES
+
+let number = 10; 
+let text = "Hello World"; 
+let isActive = false; 
+let array = [1, 2, 3]; 
+let object = { name: "Budi", age: 25 };
+
+console.log(number, text, isActive, array, object);
+
+// typeof
+console.log(typeof number);
+console.log(typeof text);
+console.log(typeof isActive);
+console.log(typeof array);  
+console.log(typeof object);
+
+
+// OPERATORS
+
+// Arithmetic
+let a = 10;
+let b = 5;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+
+
+// Comparison & Logical
+let x = 10;
+let y = 20;
+
+let comparisonResult = x < y;
+let logicalResult = (x < y && y > 5); 
+
+console.log("Comparison:", comparisonResult);
+console.log("Logical:", logicalResult);
